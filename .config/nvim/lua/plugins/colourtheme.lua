7a283100140fa518cd29f1dd7f0c90988b80fab8
JasonTulp <jason.j.tulp@gmail.com>
2025-08-05 09:27:34 +1200

    remove kanagawa colour scheme

--- .config/nvim/lua/plugins/colourtheme.lua
+++ .config/nvim/lua/plugins/colourtheme.lua
@@ -1,80 +1,89 @@
--- A nice theme
-return {
-	'rebelot/kanagawa.nvim',
-	lazy = false,
-	priority = 1000,
-	-- Load the colorscheme
-	config = function()
-		local set = vim.api.nvim_set_hl
-		local ns = 0 -- global namespace
+-- Custom colorscheme configuration
+local function setup_colorscheme()
+	local set = vim.api.nvim_set_hl
+	local ns = 0 -- global namespace
 
-		-- UI Colors
-		set(ns, 'Normal', { fg = '#d4d4d4', bg = '#212121' })
-		set(ns, 'IndentBlankLineChar', { fg = '#ff0000' })
-		set(ns, 'CursorLine', { bg = '#191919' })
-		set(ns, 'Cursor', { fg = '#212121', bg = '#569cd6' })
-		set(ns, 'Visual', { fg = '#ffffff', bg = '#353535' })
-		set(ns, 'LineNr', { fg = '#60605f', bg = '#212121' })
-		set(ns, 'CursorLineNr', { fg = '#f6f6f6', bg = '#212121' })
-		set(ns, 'Search', { bg = '#f8e71c', fg = '#212121' })
-		set(ns, 'IncSearch', { bg = '#f8e71c', fg = '#212121' })
-		set(ns, 'StatusLine', { fg = '#ffffff', bg = '#101010' })
-		set(ns, 'StatusLineNC', { fg = '#60605f', bg = '#101010' })
-		set(ns, 'VertSplit', { fg = '#e08b0b' })
-		set(ns, 'WinSeparator', { fg = '#e08b0b' })
-		set(ns, 'TabLine', { fg = '#d4d4d4', bg = '#212121' })
-		set(ns, 'TabLineSel', { fg = '#f6f6f6', bg = '#323232', bold = true })
-		set(ns, 'TabLineFill', { bg = '#191919' })
-		set(ns, 'Pmenu', { fg = '#d4d4d4', bg = '#191919' })
-		set(ns, 'PmenuSel', { bg = '#353535' })
-		set(ns, 'FloatBorder', { fg = '#e08b0b', bg = '#171717' })
-		set(ns, 'NormalFloat', { bg = '#171717' })
+	-- Clear existing colorscheme
+	vim.cmd 'highlight clear'
+	if vim.fn.exists 'syntax_on' then
+		vim.cmd 'syntax reset'
+	end
 
-		-- Syntax Colors
-		set(ns, 'Comment', { fg = '#60605f', italic = true })
-		set(ns, 'String', { fg = '#c3e88d', bold = true })
-		set(ns, 'Keyword', { fg = '#be8bdb', bold = true })
-		set(ns, 'Identifier', { fg = '#FFFFFF', bold = true }) -- variables
-		set(ns, '@variable.parameter', { fg = '#f68a68', bold = true }) -- variables
-		set(ns, '@variable', { fg = '#FFFFFF', bold = true }) -- variables
-		-- set(ns, '@variable.member', { fg = '#f68a68' }) -- variables
-		set(ns, 'Function', { fg = '#82aaff', bold = true })
-		set(ns, 'Number', { fg = '#f68a68', bold = true })
-		set(ns, 'Type', { fg = '#ffca69', bold = true })
+	-- Set colorscheme name
+	vim.g.colors_name = 'custom'
 
-		-- Types
-		set(ns, '@type.builtin', { fg = '#be8bdb', bold = true })
-		set(ns, '@type.parameter', { fg = '#f68a68', bold = true })
-		--		set(ns, '@type.identifier', { fg = '#f68a68' })
-		-- set(ns, '@constructor', { fg = '#ff0000' })
-		-- set(ns, '@function.identifier', { fg = '#ff0000' })
+	-- UI Colors
+	set(ns, 'Normal', { fg = '#d4d4d4', bg = '#212121' })
+	set(ns, 'IndentBlankLineChar', { fg = '#ff0000' })
+	set(ns, 'CursorLine', { bg = '#191919' })
+	set(ns, 'Cursor', { fg = '#212121', bg = '#569cd6' })
+	set(ns, 'Visual', { fg = '#ffffff', bg = '#353535' })
+	set(ns, 'LineNr', { fg = '#60605f', bg = '#212121' })
+	set(ns, 'CursorLineNr', { fg = '#f6f6f6', bg = '#212121' })
+	set(ns, 'Search', { bg = '#f8e71c', fg = '#212121' })
+	set(ns, 'IncSearch', { bg = '#f8e71c', fg = '#212121' })
+	set(ns, 'StatusLine', { fg = '#ffffff', bg = '#101010' })
+	set(ns, 'StatusLineNC', { fg = '#60605f', bg = '#101010' })
+	set(ns, 'VertSplit', { fg = '#e08b0b' })
+	set(ns, 'WinSeparator', { fg = '#e08b0b' })
+	set(ns, 'TabLine', { fg = '#d4d4d4', bg = '#212121' })
+	set(ns, 'TabLineSel', { fg = '#f6f6f6', bg = '#323232', bold = true })
+	set(ns, 'TabLineFill', { bg = '#191919' })
+	set(ns, 'Pmenu', { fg = '#d4d4d4', bg = '#191919' })
+	set(ns, 'PmenuSel', { bg = '#353535' })
+	set(ns, 'FloatBorder', { fg = '#e08b0b', bg = '#171717' })
+	set(ns, 'NormalFloat', { bg = '#171717' })
 
-		-- Gutter
-		set(ns, 'SignColumn', { bg = '#212121' })
-		set(ns, 'GitSignsAdd', { fg = '#c3e88d' })
-		set(ns, 'GitSignsChange', { fg = '#ffca69' })
-		set(ns, 'GitSignsDelete', { fg = '#f68a68' })
+	-- Syntax Colors
+	set(ns, 'Comment', { fg = '#60605f', italic = true })
+	set(ns, 'String', { fg = '#c3e88d', bold = true })
+	set(ns, 'Keyword', { fg = '#be8bdb', bold = true })
+	set(ns, 'Identifier', { fg = '#FFFFFF', bold = true }) -- variables
+	set(ns, '@variable.parameter', { fg = '#f68a68', bold = true }) -- variables
+	set(ns, '@variable', { fg = '#FFFFFF', bold = true }) -- variables
+	-- set(ns, '@variable.member', { fg = '#f68a68' }) -- variables
+	set(ns, 'Function', { fg = '#82aaff', bold = true })
+	set(ns, 'Number', { fg = '#f68a68', bold = true })
+	set(ns, 'Type', { fg = '#ffca69', bold = true })
 
-		-- Scrollbar (represented as MatchParen)
-		set(ns, 'MatchParen', { fg = '#ffca69', bg = '#60605f' })
+	-- Types
+	set(ns, '@type.builtin', { fg = '#be8bdb', bold = true })
+	set(ns, '@type.parameter', { fg = '#f68a68', bold = true })
+	--		set(ns, '@type.identifier', { fg = '#f68a68' })
+	-- set(ns, '@constructor', { fg = '#ff0000' })
+	-- set(ns, '@function.identifier', { fg = '#ff0000' })
 
-		-- SideBar / Panels
-		set(ns, 'NvimTreeNormal', { bg = '#191919' })
-		set(ns, 'NvimTreeWinSeparator', { fg = '#e08b0b', bg = '#191919' })
-		set(ns, 'NvimTreeVertSplit', { fg = '#e08b0b' })
-		set(ns, 'NvimTreeNormalNC', { bg = '#191919' })
+	-- Gutter
+	set(ns, 'SignColumn', { bg = '#212121' })
+	set(ns, 'GitSignsAdd', { fg = '#c3e88d' })
+	set(ns, 'GitSignsChange', { fg = '#ffca69' })
+	set(ns, 'GitSignsDelete', { fg = '#f68a68' })
 
-		-- Terminal
-		set(ns, 'TermNormal', { bg = '#191919' })
-		set(ns, 'TermCursor', { fg = '#569cd6' })
+	-- Scrollbar (represented as MatchParen)
+	set(ns, 'MatchParen', { fg = '#ffca69', bg = '#60605f' })
 
-		-- Title bar
-		set(ns, 'Title', { fg = '#ffffff', bg = '#202020' })
+	-- SideBar / Panels
+	set(ns, 'NvimTreeNormal', { bg = '#191919' })
+	set(ns, 'NvimTreeWinSeparator', { fg = '#e08b0b', bg = '#191919' })
+	set(ns, 'NvimTreeVertSplit', { fg = '#e08b0b' })
+	set(ns, 'NvimTreeNormalNC', { bg = '#191919' })
 
-		-- Section headers
-		set(ns, 'Directory', { fg = '#e08a0b', bold = true })
+	-- Terminal
+	set(ns, 'TermNormal', { bg = '#191919' })
+	set(ns, 'TermCursor', { fg = '#569cd6' })
 
-		-- Panel
-		set(ns, 'QuickFixLine', { bg = '#171717' })
-	end,
-}
+	-- Title bar
+	set(ns, 'Title', { fg = '#ffffff', bg = '#202020' })
+
+	-- Section headers
+	set(ns, 'Directory', { fg = '#e08a0b', bold = true })
+
+	-- Panel
+	set(ns, 'QuickFixLine', { bg = '#171717' })
+end
+
+-- Apply the colorscheme immediately
+setup_colorscheme()
+
+-- Return empty table to satisfy lazy.nvim plugin structure
+return {}
