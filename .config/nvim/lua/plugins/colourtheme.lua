f668d89fc44a280d6bee88da98bde30c1692314a
JasonTulp <jason.j.tulp@gmail.com>
2025-08-04 14:43:11 +1200

    update for linux

--- .config/nvim/lua/plugins/colourtheme.lua
+++ .config/nvim/lua/plugins/colourtheme.lua
@@ -1,12 +1,14 @@
 -- A nice theme
 return {
-    'rebelot/kanagawa.nvim',
-    lazy = false,
-    priority = 1000,
-    -- Load the colorscheme
-    config = function()
-      vim.cmd("colorscheme kanagawa-dragon")
-    end
+	'rebelot/kanagawa.nvim',
+	lazy = false,
+	priority = 1000,
+	-- Load the colorscheme
+	config = function()
+		vim.cmd 'colorscheme kanagawa-dragon'
+		-- Override the visual highlight to a better one
+		vim.api.nvim_set_hl(0, 'Visual', { bg = '#2d4f67' })
+		vim.api.nvim_set_hl(0, 'Cursor', { bg = '#2d4f67' })
+		vim.api.nvim_set_hl(0, 'VisualNOS', { bg = '#2d4f67' })
+	end,
 }
-
-
